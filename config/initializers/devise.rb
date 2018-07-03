c83575eb95d0f1f0a8eb2bc3d92fd84bc22a8b00
Daniel Schmidt <dcschmidt@berkeley.edu>
2018-07-03 16:47:43 -0700

    Minor cleanup to the devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,16 +1,19 @@
-Devise.setup do |config|
 require 'devise/orm/active_record'
-config.skip_session_storage = [:http_auth]
-config.stretches = Rails.env.test? ? 1 : 11
-config.reconfirmable = true
-config.expire_all_remember_me_on_sign_out = true 
-config.sign_out_via = :get
 
-calnet_url = ENV['CALNET_URL'] ? ENV['CALNET_URL']
-						: Rails.env.production? ? 'auth.berkeley.edu'
-						:													'auth-test.berkeley.edu'
+Devise.setup do |config|
+  config.skip_session_storage = [:http_auth]
+  config.stretches = Rails.env.test? ? 1 : 11
+  config.reconfirmable = true
+  config.expire_all_remember_me_on_sign_out = true
+  config.sign_out_via = :get
 
-#config.omniauth :cas,name: :altmedia,url: 'https://auth.berkeley.edu/cas/p3/serviceValidate'
-config.omniauth :cas,name: :altmedia,host: calnet_url,login_url: '/cas/login',service_validate_url: '/cas/p3/serviceValidate'
+  calnet_url = ENV.fetch('CALNET_URL') {
+    Rails.env.production? ? 'auth.berkeley.edu' : 'auth-test.berkeley.edu'
+  }
 
+  config.omniauth :cas,
+    name: :altmedia,
+    host: calnet_url,
+    login_url: '/cas/login',
+    service_validate_url: '/cas/p3/serviceValidate'
 end
