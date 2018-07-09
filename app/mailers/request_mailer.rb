2846ecd5ed7875bf91bbded2c6803161a4509bd8
Daniel Schmidt <dcschmidt@berkeley.edu>
2018-07-09 12:04:28 -0700

    Adds customizable admin email

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,36 +1,28 @@
 class RequestMailer < ActionMailer::Base
   default from: 'lib-noreply@berkeley.edu'
-#  layout 'confirmation_email'
 
-  def failure_email(empid,firstname,lastname,note)
-	  @empid = empid
+  def failure_email(empid, firstname, lastname, note)
+    @empid = empid
     @firstname = firstname
     @lastname = lastname
     @note = note
-    #email = "prntscan@lists.berkeley.edu"
-    #email = "dzuckerm@library.berkeley.edu"
-    email = ENV['EMAIL_ADDR'] 
-    mail(to: email, subject: 'alt-media scanning patron opt-in failure')
+
+    mail(subject: 'alt-media scanning patron opt-in failure')
   end
 
   def confirmation_email(email)
     mail(to: email, subject: 'alt-media scanning service opt-in')
   end
 
-  #def opt_out_staff(email,empid,firstname,lastname)
-  def opt_out_staff(empid,firstname,lastname)
+  def opt_out_staff(empid, firstname, lastname)
     @empid = empid
     @firstname = firstname
     @lastname = lastname
-    #email = "prntscan@lists.berkeley.edu"
-    #email = "dzuckerm@library.berkeley.edu"
-    email = ENV['EMAIL_ADDR'] 
-    #mail(to: email, subject: 'alt-media scanning service opt-out',body: "#{firstname} #{lastname} #{empid} has opted out of the alt-media scanning service.")
-    mail(to: email, subject: 'alt-media scanning service opt-out')
+
+    mail(subject: 'alt-media scanning service opt-out')
   end
 
   def opt_out_faculty(email)
     mail(to: email, subject: 'alt-media scanning service opt-out')
   end
-
 end
