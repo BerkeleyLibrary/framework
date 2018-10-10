771d5d695fa720e545f8c64dbabf2c01654e4d46
Daniel Schmidt <dcschmidt@berkeley.edu>
2018-10-10 11:16:12 -0700

    Adds recaptcha gem and logic (Closes #8)

--- app/controllers/ucop_borrow_request_forms_controller.rb
+++ app/controllers/ucop_borrow_request_forms_controller.rb
@@ -4,22 +4,21 @@ class UcopBorrowRequestFormsController < ApplicationController
   end
 
   def new
-    @borrow_request = UcopBorrowRequestForm.new
-
-    if not borrow_request_params.empty?
-      @borrow_request.assign_attributes(borrow_request_params)
-      @borrow_request.validate
-    end
+    init_form!
   end
 
   def create
-    @borrow_request = UcopBorrowRequestForm.new(borrow_request_params)
-    @borrow_request.submit!
+    init_form!
+    verify_recaptcha!(model: @form)
+    @form.submit!
 
-    flash_message(@borrow_request)
+    flash[:success] = t('.success', department_head_email: @form.department_head_email)
     redirect_to new_ucop_borrow_request_form_path
+  rescue Recaptcha::VerifyError
+    flash[:danger] = t('.recaptcha')
+    redirect_to_new
   rescue ActiveModel::ValidationError
-    redirect_to new_ucop_borrow_request_form_path(request.parameters)
+    redirect_to_new
   end
 
   private
@@ -30,11 +29,16 @@ class UcopBorrowRequestFormsController < ApplicationController
     {}
   end
 
-  def flash_message(borrow_request)
-    if borrow_request.valid?
-      flash[:success] = t('.success',
-        department_head_email: @borrow_request.department_head_email,
-      )
+  def init_form!
+    @form = UcopBorrowRequestForm.new
+
+    if not borrow_request_params.empty?
+      @form.assign_attributes(borrow_request_params)
+      @form.validate
     end
+  end
+
+  def redirect_to_new
+    redirect_to new_ucop_borrow_request_form_path(request.parameters)
   end
 end
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,4 @@
+Recaptcha.configure do |config|
+  config.site_key = '6LcZaXQUAAAAAGeR2GA3yfowc2k2rDXxv3B0Uul6'
+  config.skip_verify_env << 'development'
+end
