cb79f2ef21bcab2ff5faa2eb43a2514aa369b293
Daniel Schmidt <dcschmidt@berkeley.edu>
2018-10-10 16:41:29 -0700

    Move i18n test from integration to models

--- test/models/i18n_test.rb
+++ test/models/i18n_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class I18nTest < ActionDispatch::IntegrationTest
+class I18nTest < ActiveSupport::TestCase
   def test_model_translations
     assert_equal ScanRequestForm.model_name.human, 'Faculty Alt-Media Scanning'
     assert_equal UcopBorrowRequestForm.model_name.human, 'UCOP Employee Borrowing Cards'
