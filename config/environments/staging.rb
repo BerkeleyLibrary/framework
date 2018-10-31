85f4d24450ece50ec0dfa8e550fb0c4f493f9ad2
Daniel Schmidt <dcschmidt@berkeley.edu>
2018-10-31 12:17:32 -0700

    Use Rails.application.configure in the staging config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 require Rails.root.join('config/environments/production')
 require Rails.root.join('app/mailers/interceptors/staging_interceptor')
 
-Altscan::Application.configure do |config|
+Rails.application.configure do |config|
   ActionMailer::Base.register_interceptor(StagingInterceptor)
 end
