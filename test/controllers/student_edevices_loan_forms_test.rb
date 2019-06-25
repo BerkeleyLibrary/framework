ffc6c37dfd60e9746a19f4cdc82076dcd846de3e
Dan Schmidt <danschmidt5189@berkeley.edu>
2019-06-25 11:37:37 -0700

    Consolidate SLE test cases into success/failure paths

--- test/controllers/student_edevices_loan_forms_test.rb
+++ test/controllers/student_edevices_loan_forms_test.rb
@@ -1,170 +1,121 @@
 require 'test_helper'
 
 class StudentEdevicesLoanFormsControllerTest < ActionDispatch::IntegrationTest
-  #Use the stubbed records in the omniauth.yml file to simulate logins
-  setup do
-    VCR.insert_cassette 'patrons'
-  end
+  # TODO: Add a test user with SLE status who has 2 different Millennium accounts
+  ALLOWED_USERS = [
+    :ucb_grad_student,
+    :ucb_undergrad_student,
+  ]
 
-  teardown do
-    VCR.eject_cassette
-  end
+  FORBIDDEN_USERS = [
+    :ucb_blocked_faculty,
+    :ucb_scholar, # Test with a user with a PType that is not 1, 2, or 3
+    :ucb_unregistered_student,
+  ]
 
-  def test_unauthenticated_users_must_login
-    get new_student_edevices_loan_form_path
-    assert_redirected_to login_path(url: new_student_edevices_loan_form_path)
-  end
+  setup    { VCR.insert_cassette 'patrons' }
+  teardown { VCR.eject_cassette }
 
-  #For case where a user has a CalNet account but no Millennium patron record
-  def test_forbidden_if_missing_patron_record
+  # Occasionally someone will have a CalNet account for login but no Millennium
+  # patron records
+  test "forbidden_if_missing_patron_record" do
     with_login(:ucb_faculty_no_patron_record) do
       get new_student_edevices_loan_form_path
       assert_response :forbidden
     end
   end
 
-  def test_undergrad_student_user_allowed
-    with_login(:ucb_undergrad_student) do
-      get new_student_edevices_loan_form_path
-      assert_response :ok
+  test "unauthenticated_users_must_login" do
+    get new_student_edevices_loan_form_path
+    assert_redirected_to login_path(url: new_student_edevices_loan_form_path)
+  end
+
+  ALLOWED_USERS.each do |user_fixture|
+    test "#{user_fixture}_allowed" do
+      with_login(user_fixture) do |user_data|
+        get new_student_edevices_loan_form_path
+        assert_response :ok
+        assert_select "h1", /Student Electronic Devices Loan/
+        assert_select "h2", /Borrowing Guidelines/
+        assert_select "h2", /Student Lending Program Loan Period/
+        assert_select "h2", /Fines and Liability/
+        assert_select "h2", /Electronic Devices Loan Agreement/
+        assert_select "#student_edevices_loan_form_borrow_check[required=required]"
+        assert_select "#student_edevices_loan_form_lend_check[required=required]"
+        assert_select "#student_edevices_loan_form_fines_check[required=required]"
+        assert_select "#student_edevices_loan_form_edev_check[required=required]"
+        assert_select 'form input[type="submit"][value="Submit"]'
+        assert_select '.page-footer .support-email[href=?]', 'mailto:privdesk@library.berkeley.edu'
+
+        assert_select '#student_edevices_loan_form_patron_email' do
+          assert_select '[readonly=?]', 'readonly'
+          assert_select '[value=?]', user_data["info"]["email"]
+        end
+
+        assert_select '#student_edevices_loan_form_display_name' do
+          assert_select '[readonly=?]', 'readonly'
+          assert_select '[value=?]', user_data["extra"]["displayName"]
+        end
+      end
     end
   end
 
-  def test_grad_student_user_allowed
-    with_login(:ucb_grad_student) do
-      get new_student_edevices_loan_form_path
-      assert_response :ok
+  FORBIDDEN_USERS.each do |user_fixture|
+    test "#{user_fixture}_forbidden" do
+      with_login(user_fixture) do
+        get new_student_edevices_loan_form_path
+        assert_response :forbidden
+        assert_select "h1", /Forbidden/
+        assert_select "p", /The Student Electronic Devices Loan Program is only available to registered UC Berkeley Students/
+      end
     end
   end
 
-  #Test with a user with a PType that is not 1, 2, or 3
-  def test_non_student_not_allowed
-    with_login(:ucb_scholar) do
-      get new_student_edevices_loan_form_path
-      assert_response :forbidden
-    end
-  end
-
-  #Test user is a grad student with PCode2 value of 'b'
-  def test_nonregistered_student_not_allowed
-    with_login(:ucb_unregistered_student) do
-      get new_student_edevices_loan_form_path
-      assert_response :forbidden
-    end
-  end
-
-   def test_blocked_user_not_allowed
-     with_login(:ucb_blocked_faculty) do
-      get new_student_edevices_loan_form_path
-      assert_response :forbidden
-    end
-  end
-
-  def test_index_redirects_to_new
+  test "index_redirects_to_new" do
     with_login(:ucb_undergrad_student) do
       get student_edevices_loan_forms_path
       assert_redirected_to "/forms/student_edevices_loan/new"
     end
   end
 
-  #Check to see if the correct patron record is chosen when a user has a student and patron ids
-  #TODO: Add a test user with SLE status who has 2 different Millennium accounts
-  def test_valid_student_record
+  # Check to see if the correct patron record is chosen when a user has a
+  # student and patron ids
+  #
+  # TODO: This test does not seem to work with the ucb_grad_student fixture!
+  test "valid_student_record" do
     with_login(:ucb_undergrad_student) do |user_data|
       get new_student_edevices_loan_form_path
-
       assert_select '#student_edevices_loan_form_patron_id' do
         assert_select '[value=?]', user_data["extra"]["berkeleyEduStuID"]
       end
     end
   end
 
-  def test_forbidden_view_message
-    with_login(:ucb_scholar) do
-      get new_student_edevices_loan_form_path
-      assert_response :forbidden
-      assert_select "h1", /Forbidden/
-      assert_select "p", /The Student Electronic Devices Loan Program is only available to registered UC Berkeley Students/
-    end
-  end
-
-  def test_new_page_renders_with_correct_headline
+  test "required_all_boxes_checked" do
     with_login(:ucb_undergrad_student) do
-      get new_student_edevices_loan_form_path
-      assert_response :ok
-      assert_select "h1", /Student Electronic Devices Loan/
+      post "/forms/student_edevices_loan", params: params = {
+        student_edevices_loan_form: {
+          borrow_check: "unchecked",
+          lend_check: "checked",
+          fines_check: "checked",
+          edev_check: "checked",
+        }
+      }
+      assert_redirected_to new_student_edevices_loan_form_path(params)
     end
   end
 
-  def test_new_page_renders_with_correct_secondary_headers
-    with_login(:ucb_undergrad_student) do
-      get new_student_edevices_loan_form_path
-      assert_response :ok
-      assert_select "h2", /Borrowing Guidelines/
-      assert_select "h2", /Student Lending Program Loan Period/
-      assert_select "h2", /Fines and Liability/
-      assert_select "h2", /Electronic Devices Loan Agreement/
-    end
-  end
-
-  def test_questions_link_goes_to_privdesk_email
-    with_login(:ucb_undergrad_student) do
-      get new_student_edevices_loan_form_path
-      assert_select '.page-footer .support-email[href=?]',
-        'mailto:privdesk@library.berkeley.edu'
-    end
-  end
-
-  def test_patron_email_readonly
-    with_login(:ucb_undergrad_student) do |user_data|
-      get new_student_edevices_loan_form_path
-
-      assert_select '#student_edevices_loan_form_patron_email' do
-        assert_select '[readonly=?]', 'readonly'
-        assert_select '[value=?]', user_data["info"]["email"]
-      end
-    end
-  end
-
-  def test_patron_name_readonly
-    with_login(:ucb_undergrad_student) do |user_data|
-      get new_student_edevices_loan_form_path
-
-      assert_select '#student_edevices_loan_form_display_name' do
-        assert_select '[readonly=?]', 'readonly'
-        assert_select '[value=?]', user_data["extra"]["displayName"]
-      end
-    end
-  end
-
-  def test_submit_button_text
-    with_login(:ucb_undergrad_student) do
-      get new_student_edevices_loan_form_path
-      assert_select 'form input[type="submit"][value="Submit"]'
-    end
-  end
-
-  def test_success_if_all_boxes_checked_submission
+  test "success_if_all_boxes_checked_submission" do
     with_login(:ucb_grad_student) do
       post "/forms/student_edevices_loan", params: {
         student_edevices_loan_form: {
           borrow_check: "checked",
           lend_check: "checked",
           fines_check: "checked",
-          edev_check: "checked"
+          edev_check: "checked",
         }
       }
       assert_redirected_to "/forms/student_edevices_loan/all_checked"
-    end
-  end
-
-  def test_required_all_boxes_checked
-    with_login(:ucb_undergrad_student) do
-      get new_student_edevices_loan_form_path
-      assert_select "#student_edevices_loan_form_borrow_check[required=required]"
-      assert_select "#student_edevices_loan_form_lend_check[required=required]"
-      assert_select "#student_edevices_loan_form_fines_check[required=required]"
-      assert_select "#student_edevices_loan_form_edev_check[required=required]"
     end
   end
 end
