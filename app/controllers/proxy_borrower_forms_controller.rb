c80644a899781e3c4cca1ea7461d72bc1f528af2
Steve Sullivan <steve.sullivan@berkeley.edu>
2020-08-25 17:17:15 -0700

    Updating with new model name (FrameworkUsers) for updated auth setup

--- app/controllers/proxy_borrower_forms_controller.rb
+++ app/controllers/proxy_borrower_forms_controller.rb
@@ -8,7 +8,7 @@ class ProxyBorrowerFormsController < AuthenticatedFormController
     # I think I want to get the users role now... if they're in the DB
     # then I'll want to pass that info so they have the
     # admin link...otherwise, NO admin link!
-    user_role = ProxyBorrowerUsers.proxy_user_role(current_user.uid)
+    user_role = FrameworkUsers.role?(current_user.uid, 'proxyborrow_admin')
 
     faculty?
 
