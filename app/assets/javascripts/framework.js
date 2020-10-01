0151198bbb226e470e45468daa87626d1c2307e1
Steve Sullivan <steve.sullivan@berkeley.edu>
2020-10-01 13:00:24 -0700

    Fix reset buttons for stack pass and reference card forms

--- app/assets/javascripts/framework.js
+++ app/assets/javascripts/framework.js
@@ -61,6 +61,33 @@ function hardReset() {
   }
 }
 
+/**
+ * Custom Form Reset function
+ * To use add to the if/else if block your form
+ * with a list of your elements and how they should be reset
+ */
+function formReset(form) {
+  if (form === 'stack-pass') {
+    document.getElementById("stack_pass_form_name").value = "";
+    document.getElementById("stack_pass_form_email").value = "";
+    document.getElementById("stack_pass_form_phone").value = "";
+    document.getElementById("stack_pass_form_main_stack_no").checked = false;
+    document.getElementById("stack_pass_form_main_stack_yes").checked = false;
+    toggleBlock("main_stack_warn", false);
+    document.getElementById("stack_pass_form_pass_date").value = "";
+    document.getElementById("stack_pass_form_local_id").value = "";
+  } else if (form === 'reference-card') {
+    document.getElementById("reference_card_form_name").value = "";
+    document.getElementById("reference_card_form_email").value = "";
+    document.getElementById("reference_card_form_affiliation").value = "";
+    document.getElementById("reference_card_form_research_desc").value = "";
+    document.getElementById("reference_card_form_local_id").value = "";
+    document.getElementById("reference_card_form_pass_date").value = "";
+    document.getElementById("reference_card_form_pass_date_end").value = "";
+  }
+
+}
+
 // Stack Pass approve/request page:
 // Control the submit button disable/enable
 function sp_proccess_check() {
