f1f61d55e5138820c29261e771475564df211dc5
David Moles <dmoles@berkeley.edu>
2021-03-12 11:58:22 -0800

    Make CSV download actually work

--- app/controllers/tind_download_controller.rb
+++ app/controllers/tind_download_controller.rb
@@ -21,19 +21,20 @@ class TindDownloadController < ApplicationController
     render locals: { root_collections: root_collections }
   end
 
-  # TODO: require a collection
+  # TODO: user-friendly error message for missing parameters
   # TODO: validate that collection exists (TIND will just 500 on us)
   # TODO: prompt w/collection name & number of records
   # TODO: figure out how to update the page after downloading
   def download
-    # TODO: Is there any way to 'start' the downlod before we actually
-    #       build the data, so the user thinks something's happening?
-    data = UCBLIT::TIND::Export.export_libreoffice(collection_name)
-    send_data(
-      data,
-      filename: "#{collection_name.parameterize}.ods",
-      type: 'application/vnd.oasis.opendocument.spreadsheet'
-    )
+    filename = "#{collection_name.parameterize}.#{export_format}"
+    content_type = export_format.mime_type
+
+    # "Start" the download before we actually generate the data, so
+    # it looks like something's happening
+    send_file_headers!(filename: filename, type: content_type)
+
+    data = UCBLIT::TIND::Export.export(collection_name, export_format)
+    render(body: data, content_type: content_type)
   end
 
   def find_collection
@@ -86,8 +87,29 @@ class TindDownloadController < ApplicationController
     distances[0...NAME_MATCH_COUNT].map { |_, v| v }
   end
 
+  def export_params
+    @export_params ||= begin
+      logger.debug(params: params)
+      required_params = %i[collection_name export_format]
+      params.tap do |pp|
+        # :format is a default parameter added from routes.rb
+        # TODO: do we still need this?
+        pp.permit(required_params + %i[format])
+        required_params.each { |p| pp.require(p) }
+      end
+    end
+  end
+
   def collection_name
-    params[:collection_name]
+    export_params[:collection_name]
+  end
+
+  # @return [UCBLIT::TIND::Export::ExportFormat] the selected format
+  def export_format
+    # noinspection RubyYardParamTypeMatch
+    fmt_param = export_params[:export_format]
+    logger.debug("format: #{fmt_param.inspect} (as string: #{fmt_param.to_s.inspect})")
+    UCBLIT::TIND::Export::ExportFormat.ensure_format(fmt_param)
   end
 
 end
