165b74836487faa79f4a4cee67625494ccd09a09
Steve Sullivan <steve.sullivan@berkeley.edu>
2021-03-31 09:54:42 -0700

    Initial stab at tests

---
+++ spec/request/tind_download_request_spec.rb
@@ -0,0 +1,33 @@
+require 'forms_helper'
+
+describe 'TIND Download Request Form', type: :request do
+  attr_reader :patron_id
+  attr_reader :patron
+  attr_reader :user
+
+  context 'specs for non-staff user' do
+    it 'page redirects' do
+      get tind_download_path
+      expect(response.status).to eq 302
+    end
+  end
+
+  context 'specs for staff user' do
+    before(:each) do
+      @patron_id = Patron::Type.sample_id_for(Patron::Type::STAFF)
+      @user = login_as(patron_id)
+      @patron = Patron::Record.find(patron_id)
+
+      stub_request(:get, "https://digicoll.lib.berkeley.edu/api/v1/collections?depth=100").to_return(
+        status: 200,
+        body: File.new('spec/data/tindcollections.json')
+      )
+    end
+
+    it 'form opens' do
+      get tind_download_path
+      expect(response.status).to eq 200
+      expect(response.body).to include('<h1>TIND Metadata Download</h1>')
+    end
+  end
+end
