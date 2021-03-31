3d03057ed5d0deacfcd45bec582975f24362f469
Steve Sullivan <steve.sullivan@berkeley.edu>
2021-03-31 09:47:08 -0700

    Adding some JS to handle TIND downloader form submission and enable/disable submit button

--- app/assets/javascripts/tind_collection_autocomplete.js
+++ app/assets/javascripts/tind_collection_autocomplete.js
@@ -1,6 +1,6 @@
 // TODO: add some sort of spinner to show we're working
 
-$(function() {
+$(function () {
     // TODO: highlight *unmatched* text
     //   cf. https://stackoverflow.com/questions/9887032/how-to-highlight-input-words-in-autocomplete-jquery-ui
     // https://baymard.com/blog/autocomplete-design
@@ -8,3 +8,34 @@ $(function() {
         source: "/tind-download/find_collection"
     })
 })
+
+// Used to enable/disable the submit button - no collection name, no submit!
+function checkCollectionValue() {
+    let col_name = document.getElementById('collection_name').value || null;
+
+    if (col_name) {
+        toggleDisable('export-btn', false);
+    } else {
+        toggleDisable('export-btn', true);
+    }
+}
+
+// Submit the form, after a few odds and ends that are easier to do on the front end
+function downloadFile() {
+
+    // Clear out any flash alerts that user may have gotten before submitting the file:
+    let flash_alert = document.getElementsByClassName('alert')[0] || null;
+
+    if (flash_alert) {
+        document.getElementsByClassName('alert')[0].style.display = "none";
+    }
+
+    // Before downloading, change the page to show the 'your download should start soon'
+    // message since rails doesn't allow you to download and then render the page:
+    toggleBlock('response-div', true);
+    toggleBlock('form-div', false);
+    toggleBlock('description-text', false);
+
+    // NOW submit the form which should kick off the download:
+    document.forms["down-form"].submit();
+}
