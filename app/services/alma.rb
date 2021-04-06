a9ebbe2f5c98c0a021b36990d30ae2005557acab
Steve Sullivan <steve.sullivan@berkeley.edu>
2021-04-06 13:04:40 -0700

    tweaks for rubocop

--- app/services/alma.rb
+++ app/services/alma.rb
@@ -1,9 +1,9 @@
 module Alma
-  BASE_URL = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/'
+  BASE_URL = 'https://api-na.hosted.exlibrisgroup.com/almaws/v1/'.freeze
   class Fines
     def self.fetch_all(alma_id)
       request = "#{BASE_URL}users/#{alma_id}/fees?apikey=#{ENV['ALMA_KEY']}"
-      Faraday.get(request,{},{'Accept' => 'application/json'})
+      Faraday.get(request, {}, { 'Accept' => 'application/json' })
     end
   end
 end
