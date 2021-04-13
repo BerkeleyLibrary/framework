19ec4cc0be33abc32b14095c5b6cef88b768faf1
David Moles <dmoles@berkeley.edu>
2021-04-13 11:02:19 -0700

    Just recreate models for every test (it's not that expensive) so they get cleaned up properly

--- spec/system/framework_user_system_spec.rb
+++ spec/system/framework_user_system_spec.rb
@@ -3,24 +3,17 @@ require 'capybara_helper'
 describe :forms_proxy_borrower_admin, type: :system do
 
   context 'specs with hardcoded admin' do
-    # First create a DSP Rep and assignment
-    before(:all) do
-      # Create User, Role and Assignment:
+    before(:each) do
+      # First create a DSP Rep and assignment
       @user = FrameworkUsers.create(id: 1, lcasid: 112_233, name: 'John Doe', role: 'Admin')
       @role = Role.create(id: 1, role: 'proxyborrow_admin')
       @assignment = Assignment.create(framework_users_id: 1, role_id: 1)
-    end
 
-    after(:all) do
-      [@assignment, @role, @user].each(&:destroy)
-    end
-
-    # Go to the Admin Users View Page:
-    before(:each) do
       # These functions require admin privledges:
       admin_user = User.new(uid: '1707532')
       allow_any_instance_of(ProxyBorrowerAdminController).to receive(:current_user).and_return(admin_user)
 
+      # Go to the Admin Users View Page:
       visit forms_proxy_borrower_admin_users_path
     end
 
@@ -45,26 +38,18 @@ end
 
 describe :forms_stack_pass_admin, type: :system do
   context 'specs with hardcoded admin' do
-    # First create an admin and assignment
-    before(:all) do
-      # Clear the way:
-      Assignment.delete_all
-      Role.delete_all
-      FrameworkUsers.delete_all
-
-      # Create User, Role and Assignment:
+    before(:each) do
+      # First create an admin and assignment
       FrameworkUsers.create(id: 1, lcasid: 112_233, name: 'John Doe', role: 'Admin')
       Role.create(id: 1, role: 'proxyborrow_admin')
       Role.create(id: 2, role: 'stackpass_admin')
       Assignment.create(framework_users_id: 1, role_id: 2)
-    end
 
-    # Go to the Admin Users View Page:
-    before(:each) do
       # These functions require admin privledges:
       admin_user = User.new(uid: '1707532')
       allow_any_instance_of(StackPassAdminController).to receive(:current_user).and_return(admin_user)
 
+      # Go to the Admin Users View Page:
       visit forms_stack_pass_admin_users_path
     end
 
