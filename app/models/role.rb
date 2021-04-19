70e04fe0737686b63a1a024cc21c25be59de5f3b
David Moles <dmoles@berkeley.edu>
2021-04-19 14:50:54 -0700

    Stop hard-coding role IDs

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,4 +1,22 @@
 class Role < ActiveRecord::Base
   has_many :assignments
   has_many :framework_users, through: :assignments
+
+  class << self
+    def proxyborrow_admin
+      Role.find_or_create_by(role: 'proxyborrow_admin')
+    end
+
+    def proxyborrow_admin_id
+      proxyborrow_admin.id
+    end
+
+    def stackpass_admin
+      Role.find_or_create_by(role: 'stackpass_admin')
+    end
+
+    def stackpass_admin_id
+      stackpass_admin.id
+    end
+  end
 end
