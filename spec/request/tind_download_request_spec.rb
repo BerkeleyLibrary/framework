99c311f507449399a0eca3b2c5732952a7954586
David Moles <dmoles@berkeley.edu>
2021-04-22 15:34:08 -0700

    Separate authenticated and unauthenticated user specs

--- spec/request/tind_download_request_spec.rb
+++ spec/request/tind_download_request_spec.rb
@@ -5,19 +5,36 @@ describe 'TIND Download Request Form', type: :request do
   attr_reader :patron
   attr_reader :user
 
-  context 'specs for non-staff user' do
+  context 'specs for unauthenticated user' do
+    before(:each) do
+      logout! # just to be sure
+    end
+
     it 'page redirects' do
+      get(form_path = tind_download_path)
+      expect(response).to redirect_to("#{login_path}?#{URI.encode_www_form(url: form_path)}")
+    end
+  end
+
+  context 'specs for non-staff user' do
+    around(:each) do |example|
+      patron_id = Patron::Type.sample_id_for(Patron::Type::FACULTY)
+      with_patron_login(patron_id) { example.run }
+    end
+
+    it 'returns 403' do
       get tind_download_path
-      expect(response.status).to eq 302
+      expect(response.status).to eq(403)
     end
   end
 
   context 'specs for staff user' do
-    before(:each) do
-      @patron_id = Patron::Type.sample_id_for(Patron::Type::STAFF)
-      @user = login_as_patron(patron_id)
-      @patron = Patron::Record.find(patron_id)
+    around(:each) do |example|
+      patron_id = Patron::Type.sample_id_for(Patron::Type::STAFF)
+      with_patron_login(patron_id) { example.run }
+    end
 
+    before(:each) do
       stub_request(:get, 'https://digicoll.lib.berkeley.edu/api/v1/collections?depth=100').to_return(
         status: 200,
         body: File.new('spec/data/tindcollections.json')
