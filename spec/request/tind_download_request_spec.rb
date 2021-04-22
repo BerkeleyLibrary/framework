8d9760c8e438f3129ac497a4dfeec6dc6d84ca1d
David Moles <dmoles@berkeley.edu>
2021-04-22 14:58:54 -0700

    Fix spec

--- spec/request/tind_download_request_spec.rb
+++ spec/request/tind_download_request_spec.rb
@@ -15,7 +15,7 @@ describe 'TIND Download Request Form', type: :request do
   context 'specs for staff user' do
     before(:each) do
       @patron_id = Patron::Type.sample_id_for(Patron::Type::STAFF)
-      @user = login_as(patron_id)
+      @user = login_as_patron(patron_id)
       @patron = Patron::Record.find(patron_id)
 
       stub_request(:get, 'https://digicoll.lib.berkeley.edu/api/v1/collections?depth=100').to_return(
