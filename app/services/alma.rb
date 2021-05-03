e5970bbb5c2f60064a74f59f6f33214f81cdd2aa
Steve Sullivan <steve.sullivan@berkeley.edu>
2021-05-03 12:00:48 -0700

    Tweaks to make rubocop happy(er)

--- app/services/alma.rb
+++ app/services/alma.rb
@@ -11,23 +11,23 @@ module Alma
       request = "#{BASE_URL}users/#{alma_id}/fees/#{fine_id}?apikey=#{ENV['ALMA_KEY']}"
       Faraday.get(request, {}, { 'Accept' => 'application/json' })
     end
-    
+
     def self.credit_fine(alma_id, fine_id)
       # NOTE - if you pay the full amount owed for a fee, it automatically changes the status to "CLOSED"
-      
+
       # comment (query) - optional
       # method (query) - (required if op=pay) Options are : CREDIT_CARD, ONLINE, CASH
-      # amount (query) - req if pay 
+      # amount (query) - req if pay
       # op (query) - required - pay, waive, dispute, restore
       amount = '1.00'
       comment = 'Test2'
       paypal_id = 'NA'
 
-      request = "#{BASE_URL}users/#{alma_id}/fees/#{fine_id}?apikey=#{ENV['ALMA_KEY']}&op=pay&amount=#{amount}&method=ONLINE&comment=#{comment}&external_transaction_id=#{paypal_id}"
+      request = "#{BASE_URL}users/#{alma_id}/fees/#{fine_id}?apikey=#{ENV['ALMA_KEY']}
+                 &op=pay&amount=#{amount}&method=ONLINE&comment=#{comment}&external_transaction_id=#{paypal_id}"
 
       puts "\n\nREQUEST:\n>#{request}<\n\n"
-      #Faraday.post(request, {}, { 'Accept' => 'application/json' })
-      
+      # Faraday.post(request, {}, { 'Accept' => 'application/json' })
     end
   end
 end
