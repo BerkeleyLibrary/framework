002f01fba52b4a9b8726034446d78f37bb5a60cf
David Moles <dmoles@berkeley.edu>
2021-06-01 11:46:06 -0700

    Add LendingItem attribute validations, fix tests

--- app/models/lending_item.rb
+++ app/models/lending_item.rb
@@ -1,2 +1,17 @@
 class LendingItem < ActiveRecord::Base
+  validates :barcode, presence: true
+  validates :filename, presence: true
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :copies, numericality: { greater_than_or_equal_to: 0 }
+  validate :ils_record_present
+
+  # ------------------------------------------------------------
+  # Custom validation methods
+
+  def ils_record_present
+    ils_record_fields = [:millennium_record, :alma_record]
+    return if ils_record_fields.any? { |f| send(f).present? }
+    errors.add(:base, "At least one ILS record ID (#{ils_record_fields.join(', ')} must be present")
+  end
 end
