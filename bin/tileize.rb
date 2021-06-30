69f1caca9e65dddedb4281d964b3a7e18af962cb
David Moles <dmoles@berkeley.edu>
2021-06-30 15:16:40 -0700

    Add plain Ruby script to tileize files

---
+++ bin/tileize.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+# Require gems
+require 'bundler/setup'
+
+# Require lib/lending
+unless $LOAD_PATH.include?((lib_path = File.expand_path('../lib', __dir__)))
+  puts "Adding #{lib_path} to $LOAD_PATH"
+  $LOAD_PATH.unshift(lib_path)
+end
+require 'lending'
+
+# Parse arguments
+indir, outdir = ARGV
+[indir, outdir].each { |d| raise ArgumentError, "Not a directory: #{d}" unless File.directory?(d) }
+
+# Tileize files
+Lending::Tileizer.tileize_all(indir, outdir, skip_existing: true)
