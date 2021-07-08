010fd2f1db9450bab6e6d64c6823b0f3e3b06016
David Moles <dmoles@berkeley.edu>
2021-07-08 15:27:59 -0700

    Restore standalone tileize script

---
+++ bin/tileize.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+# Require gems
+require 'bundler/setup'
+
+# Require lib/lending
+unless $LOAD_PATH.include?((lib_path = File.expand_path('../lib', __dir__)))
+  puts "Adding #{lib_path} to $LOAD_PATH"
+  $LOAD_PATH.unshift(lib_path)
+end
+require 'lending'
+
+# Parse arguments
+infile, outfile = ARGV
+[infile, outfile].each { |d| raise ArgumentError, "#{d}: No such file or directory" unless File.exist?(d) }
+
+if File.directory?(outfile)
+  Lending::Tileizer.tileize_all(infile, outfile, skip_existing: true)
+else
+  Lending::Tileizer.tileize(infile, outfile)
+end
