3ad97aac3e431469a0a45e06d6c9ee625e69a622
David Zuckerman <davidzuckerman@Davids-iMac.local>
2021-09-22 12:45:18 -0700

    removed disabling of rubocopy Metrics/AbcSize

--- app/services/concerns/valid_proxy_patron.rb
+++ app/services/concerns/valid_proxy_patron.rb
@@ -2,14 +2,12 @@ module ValidProxyPatron
 
   class << self
 
-    # rubocop:disable Metrics/AbcSize
     def valid?(patron)
       patron['status']['value'] == 'ACTIVE' &&
       patron['user_block'].empty? &&
       fees_less_than?(patron['fees']['value'].to_f, 50) &&
       valid_group?(patron['user_group']['value'])
     end
-    # rubocop:enable Metrics/AbcSize
 
     def fees_less_than?(fee, max)
       fee < max
