d8eef96f9e1114cd760244f4e0f6de7981137db0
David Moles <dmoles@dmoles.net>
2022-11-16 11:57:57 -0800

    First bash at updating to Rails 7, updating other deps

--- Gemfile
+++ Gemfile
@@ -6,9 +6,10 @@ ruby File.read('.ruby-version').strip
 
 gem 'awesome_print', '>=1.8.0'
 gem 'berkeley_library-docker', '~> 0.2.0'
-gem 'bootsnap', '~> 1.7', '>= 1.7.4', require: false
+gem 'berkeley_library-logging', '~> 0.2', '>= 0.2.7'
+gem 'berkeley_library-marc', '~> 0.3.1'
+gem 'berkeley_library-tind', '~> 0.7.0'
 gem 'bootstrap'
-gem 'coffee-rails', '~> 5.0'
 gem 'faraday'
 gem 'ipaddress'
 gem 'jaro_winkler', '~> 1.5.4'
@@ -19,21 +20,16 @@ gem 'jwt', '~> 1.5', '>= 1.5.4'
 gem 'lograge', '>=0.11.2'
 gem 'net-ssh'
 gem 'netaddr', '~> 1.5', '>= 1.5.1'
-gem 'omniauth-cas',
-    git: 'https://github.com/dlindahl/omniauth-cas.git',
-    ref: '7087bda829e14c0f7cab2aece5045ad7015669b1'
-gem 'ougai', '>=1.8.2'
+gem 'omniauth', '~> 1.9', '>= 1.9.2'
+gem 'omniauth-cas', '~> 2.0'
 gem 'pg', '~> 1.2'
 gem 'prawn', '~> 2.4'
 gem 'puma', '~> 4.3', '>= 4.3.12'
-gem 'rails', '~> 6.0.3'
+gem 'rails', '~> 7.0.4'
 gem 'recaptcha', '~> 4.13'
-gem 'turbolinks', '~> 5'
+gem 'sprockets', '~> 4.0'
 gem 'typesafe_enum', '~> 0.3'
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
-gem 'ucblit-logging', git: 'https://git.lib.berkeley.edu/lap/ucblit-logging.git', branch: 'main'
-gem 'ucblit-marc', git: 'https://git.lib.berkeley.edu/lap/ucblit-marc.git', branch: 'main'
-gem 'ucblit-tind', '>= 0.3.3', git: 'https://git.lib.berkeley.edu/lap/ucblit-tind.git', branch: 'main'
 gem 'uglifier', '>= 1.3.0'
 
 group :development, :test do
@@ -44,10 +40,9 @@ group :development, :test do
   gem 'database_cleaner-active_record', '~> 2.0'
   gem 'dotenv', '~> 2.7'
   gem 'roo', '~> 2.8'
-  gem 'rspec'
-  gem 'rspec-rails'
-  gem 'rspec-support'
-  gem 'rspec_junit_formatter'
+  gem 'rspec', '~> 3.10'
+  gem 'rspec_junit_formatter', '~> 0.5'
+  gem 'rspec-rails', '~> 5.0'
   gem 'ruby-prof', '~> 0.17.0'
   gem 'vcr'
   gem 'webmock'
@@ -65,6 +60,6 @@ group :test do
   gem 'capybara', '~> 3.36'
   gem 'concurrent-ruby', '~> 1.1'
   gem 'selenium-webdriver', '~> 4.0'
-  gem 'simplecov', '~> 0.16.1', require: false
-  gem 'simplecov-rcov', require: false
+  gem 'simplecov', '~> 0.21', require: false
+  gem 'simplecov-rcov', '~> 0.2', require: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,24 +2,23 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_174707) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_11_18_174707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "assignments", force: :cascade do |t|
     t.bigint "framework_users_id", null: false
     t.bigint "role_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["framework_users_id"], name: "index_assignments_on_framework_users_id"
     t.index ["role_id"], name: "index_assignments_on_role_id"
   end
@@ -44,15 +43,15 @@ ActiveRecord::Schema.define(version: 2020_11_18_174707) do
     t.date "date_term"
     t.integer "renewal", default: 0
     t.integer "status", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "user_email"
   end
 
   create_table "roles", force: :cascade do |t|
     t.string "role", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "stack_requests", force: :cascade do |t|
@@ -65,8 +64,8 @@ ActiveRecord::Schema.define(version: 2020_11_18_174707) do
     t.boolean "approvedeny"
     t.string "processed_by"
     t.string "denial_reason"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "type"
     t.string "affiliation"
     t.text "research_desc"
