b484fd5625129bbf43536f5e47aa770a32e8cbdf
David Moles <dmoles@dmoles.net>
2023-03-17 11:53:40 -0700

    Add test for kill/resume

--- spec/jobs/holdings/world_cat_job_spec.rb
+++ spec/jobs/holdings/world_cat_job_spec.rb
@@ -54,13 +54,17 @@ module Holdings
       }
     end
 
+    def stub_request_for(oclc_number)
+      req = BerkeleyLibrary::Holdings::WorldCat::LibrariesRequest.new(oclc_number)
+      xml = File.read("spec/data/holdings/world_cat/#{oclc_number}.xml")
+      stub_request(:get, req.uri).with(query: req.params).to_return(body: xml)
+    end
+
     it 'retrieves the holdings' do
       task_wc_records = task.holdings_world_cat_records
 
       oclc_numbers_expected.each do |oclc_number|
-        req = BerkeleyLibrary::Holdings::WorldCat::LibrariesRequest.new(oclc_number)
-        xml = File.read("spec/data/holdings/world_cat/#{oclc_number}.xml")
-        stub_request(:get, req.uri).with(query: req.params).to_return(body: xml)
+        stub_request_for(oclc_number)
       end
 
       WorldCatJob.perform_now(task)
@@ -82,9 +86,7 @@ module Holdings
 
       oclc_numbers_expected.each_with_index do |oclc_number, i|
         if i.odd?
-          req = BerkeleyLibrary::Holdings::WorldCat::LibrariesRequest.new(oclc_number)
-          xml = File.read("spec/data/holdings/world_cat/#{oclc_number}.xml")
-          stub_request(:get, req.uri).with(query: req.params).to_return(body: xml)
+          stub_request_for(oclc_number)
         else
           symbols_expected = holdings_by_oclc_num[oclc_number]
           wc_record = task_wc_records.find_by!(oclc_number:)
@@ -110,11 +112,10 @@ module Holdings
       task_wc_records = task.holdings_world_cat_records
 
       oclc_numbers_expected.each_with_index do |oclc_number, i|
-        req = BerkeleyLibrary::Holdings::WorldCat::LibrariesRequest.new(oclc_number)
         if i.odd?
-          xml = File.read("spec/data/holdings/world_cat/#{oclc_number}.xml")
-          stub_request(:get, req.uri).with(query: req.params).to_return(body: xml)
+          stub_request_for(oclc_number)
         else
+          req = BerkeleyLibrary::Holdings::WorldCat::LibrariesRequest.new(oclc_number)
           stub_request(:get, req.uri).with(query: req.params).to_return(status: 500)
         end
       end
@@ -136,6 +137,47 @@ module Holdings
           expect(wc_record.wc_symbols).to be_nil
           expect(wc_record.wc_error).to eq('500 Internal Server Error')
         end
+      end
+    end
+
+    it '"handles" being interrupted/killed and then resumed' do
+      midpoint = oclc_numbers_expected.size / 2
+      first_batch = oclc_numbers_expected[0...midpoint]
+      second_batch = oclc_numbers_expected[midpoint..]
+
+      # Simulate being killed after retrieving half the items
+      first_batch.each do |oclc_number|
+        stub_request_for(oclc_number)
+      end
+
+      oclc_number_midpoint = oclc_numbers_expected[midpoint]
+      req_midpoint = BerkeleyLibrary::Holdings::WorldCat::LibrariesRequest.new(oclc_number_midpoint)
+      stub_request(:get, req_midpoint.uri).with(query: req_midpoint.params).to_raise(SignalException.new(:KILL))
+
+      expect do
+        WorldCatJob.perform_now(task)
+      end.to raise_error(SignalException)
+
+      task_wc_records = task.holdings_world_cat_records
+      expect(task_wc_records.where(retrieved: true).count).to eq(first_batch.size)
+      expect(task_wc_records.where.not(wc_error: nil)).not_to exist
+
+      # Simulate retry after interrupt
+      second_batch.each do |oclc_number|
+        stub_request_for(oclc_number)
+      end
+
+      WorldCatJob.perform_now(task)
+
+      expected_count = holdings_by_oclc_num.size
+      expect(task_wc_records.where(retrieved: true).count).to eq(expected_count)
+      expect(task_wc_records.where(retrieved: false)).not_to exist
+
+      task_wc_records.find_each do |wc_record|
+        oclc_number = wc_record.oclc_number
+        symbols_expected = holdings_by_oclc_num[oclc_number]
+        symbols_actual = wc_record.wc_symbols.split(',')
+        expect(symbols_actual).to contain_exactly(*symbols_expected)
       end
     end
   end
