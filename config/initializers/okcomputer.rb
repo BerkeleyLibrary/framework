68a3fcd2ae455ced61448e84f6cf2d0ea8d211ad
David Zuckerman <dzuckerm@library.berkeley.edu>
2026-01-29 18:59:16 -0800

    added connection checks for smtp in okcomputer

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -18,17 +18,39 @@ class AlmaPatronCheck < OkComputer::Check
   end
 end
 
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 class CustomMailerCheck < OkComputer::Check
+  require 'net/smtp'
   # Check that the mail password is set
   def check
-    if ENV['MAIL_PASSWORD'].present?
-      mark_message 'Environment variable MAIL_PASSWORD is set.'
-    else
+    settings = ActionMailer::Base.smtp_settings
+    begin
+      Net::SMTP.start(
+        settings[:address],
+        settings[:port],
+        settings[:domain],
+        settings[:user_name],
+        settings[:password],
+        settings[:authentication],
+        tls: true
+      ) { mark_message 'Connection for smtp successful' }
+    rescue Net::SMTPAuthenticationError => e
       mark_failure
-      mark_message 'Environment variable MAIL_PASSWORD is not set!'
+      mark_message "Authentication error: #{e.message}"
+    rescue Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
+      mark_failure
+      mark_message "SMTP error: #{e.message}"
+    rescue IOError, Net::ReadTimeout => e
+      mark_failure
+      mark_message "Connection error: #{e.message}"
+    rescue StandardError => e
+      # Catch any other unexpected errors
+      mark_failure
+      mark_message "An unexpected error occurred: #{e.message}"
     end
   end
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 # Ensure Alma API is working.
 OkComputer::Registry.register 'alma-patron-lookup', AlmaPatronCheck.new
