9dfe69d5e31295f2f435789b7bfa8a959da35fb6
zhouyu <zhouyu@berkeley.edu>
2026-02-05 14:31:46 -0800

    Add additional health checks for external services

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Health check configuration
+require 'berkeley_library/util/uris/head_check'
 
 OkComputer.logger = Rails.logger
 OkComputer.check_in_parallel = true
@@ -26,3 +27,21 @@ OkComputer::Registry.register 'database-migrations', OkComputer::ActiveRecordMig
 
 # Ensure connectivity to the mail system.
 OkComputer::Registry.register 'action-mailer', OkComputer::ActionMailerCheck.new
+
+# Ensure TIND API is working.
+tind_health_check_url = "#{Rails.application.config.tind_base_uri}api/v1/search?In=en"
+OkComputer::Registry.register 'thind-api', BerkeleyLibrary::Util::HeadCheck.new(tind_health_check_url)
+
+# Ensure HathiTrust API is working.
+OkComputer::Registry.register 'hathitrust-api', BerkeleyLibrary::Util::HeadCheck.new(Rails.application.config.hathiTrust_health_check_url)
+
+# Ensure ARIN Whois API is working.
+OkComputer::Registry.register 'whois-arin-api', BerkeleyLibrary::Util::HeadCheck.new(Rails.application.config.whois_health_check_url)
+
+# Ensure Berkeley ServiceNow is accessible.
+OkComputer::Registry.register 'berkeley-service-now', BerkeleyLibrary::Util::HeadCheck.new(Rails.application.config.berkeley_service_now_health_check_url)
+
+# Ensure PayPal Payflow is accessible.
+OkComputer::Registry.register 'paypal-payflow', OkComputer::HttpCheck.new(Rails.application.config.paypal_payflow_url)
+
+# Since the WorldCat API service requests dynamically generated OCLC tokens, we are not doing a health check for it.
--- spec/request/okcomputer_spec.rb
+++ spec/request/okcomputer_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'OKComputer', type: :request do
-  before { allow(Alma::User).to receive(:find).and_return(Alma::User.new) }
+  before do
+    allow(Alma::User).to receive(:find).and_return(Alma::User.new)
+    tind_health_check_url = "#{Rails.application.config.tind_base_uri}api/v1/search?In=en"
+    stub_request(:head, tind_health_check_url).to_return(status: 200)
+    stub_request(:head, Rails.application.config.whois_health_check_url).to_return(status: 200)
+    stub_request(:head, Rails.application.config.hathiTrust_health_check_url).to_return(status: 200)
+    stub_request(:head, Rails.application.config.berkeley_service_now_health_check_url).to_return(status: 200)
+    stub_request(:get, Rails.application.config.paypal_payflow_url).to_return(status: 200)
+  end
 
   it 'is mounted at /okcomputer' do
     get '/okcomputer'
@@ -9,6 +17,7 @@ RSpec.describe 'OKComputer', type: :request do
   end
 
   it 'returns all checks to /health' do
+
     get '/health'
     expect(response.parsed_body.keys).to match_array %w[
       action-mailer
@@ -16,6 +25,11 @@ RSpec.describe 'OKComputer', type: :request do
       default
       database
       database-migrations
+      thind-api
+      whois-arin-api
+      paypal-payflow
+      hathitrust-api
+      berkeley-service-now
     ]
     pending 'https://github.com/emmahsax/okcomputer/pull/21'
     expect(response).to have_http_status :ok
