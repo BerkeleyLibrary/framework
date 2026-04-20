6299edec13a4b39757eceb9d0718b94be2ac96b6
David Zuckerman <dzuckerm@berkeley.edu>
2026-04-20 13:17:11 -0700

    Added some comments for header validation description for tind spread validator

--- app/lib/tind_spread/tind_validation.rb
+++ app/lib/tind_spread/tind_validation.rb
@@ -4,7 +4,10 @@ module TindSpread
   # rubocop:disable Metrics/ModuleLength
   module TindValidation
 
-    # validates the header rown
+    # validates the header row
+    # should be 3 digits for field, 2 for indicator (can be _, number), one digit or number for subfield
+    # optionally can have a ('-' followed by a number). This is used to group columns into similar fields
+    # the header row can also be just "Filename" or "FFT". The program will create the proper fields for those
     def self.valid_header?(str)
       str.match?(/\d{3}[_|\d]{2}[a-zA-Z0-9]$/) || str.match?(/\d{3}[_|\d]{2}[a-zA-Z0-9]-\d$/) || str.match?(/Filename|FFT/i)
     end
